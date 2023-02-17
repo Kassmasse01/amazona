2fce1da4f2cfef5389bcbe3d08e7934315304ec3
Kassmasse01 <kasget1@gmail.com>
2023-02-17 14:26:43 +0300

    video-08-handle-add-to-cart

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useContext } from "react";
 import Head from "next/head";
 import Link from "next/link";
+import { Store } from "@/utils/Store";
 
 export default function Layout({title,children}) {
+    const { state } = useContext(Store);
+    const { cart } = state;
+    
     return (
         <>
         <Head>
@@ -18,7 +22,17 @@ export default function Layout({title,children}) {
                         amazona
                     </Link>
                     <div>
-                        <Link href="/cart">Cart</Link>
+                        <Link href="/cart" className="p-2">                          
+            
+                            
+                                 Cart
+                                 {cart.cartItems.length > 0 && (
+                                <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                                   {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                                </span>
+                  )}
+                
+                            </Link>
                         <Link href="/Login">Login</Link>
                     </div>
                 </nav>
